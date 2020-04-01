ec556221bde5e5d1663e68001e5ddcf8376908d5
Paul Leone <p-leone@hotmail.com>
2020-03-31 18:30:10 -0700

    Deploy failed due to creating own Port. Fixed issue

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-app.listen(7000, () => {
-    console.log("Listening on port 7000")
+const PORT = process.env.PORT || 7000
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 });
