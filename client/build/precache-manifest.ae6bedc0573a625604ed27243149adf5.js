6ef428333e2a47ecc97ab01d8e20c255b86a30e0
Paul Leone <p-leone@hotmail.com>
2020-03-31 20:07:52 -0700

    deploy ready

---
+++ client/build/precache-manifest.ae6bedc0573a625604ed27243149adf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6444f8211d09416c207bb86ce4c1de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b4020bf0d189bec2b8d",
+    "url": "/static/css/main.ceb1fc35.chunk.css"
+  },
+  {
+    "revision": "d9b694879e437b7f9433",
+    "url": "/static/js/2.f064e3a9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f064e3a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4020bf0d189bec2b8d",
+    "url": "/static/js/main.e6373cda.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
